52998c69e7efb73f2d1e46348d12b1dab33a9ddb
KnightParzivalll <koffection@gmail.com>
2024-04-17 20:14:25 +0300

    [with_mysql] refactor: delete image module and datasource module feat: add typeorm to use database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,27 @@
 import { Module, ValidationPipe } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
-import { DatasourceModule } from './datasource/datasource.module';
 import { RentProductsModule } from './rentProducts/rentProducts.module';
 import { FeedbackModule } from './feedback/feedback.module';
 import { RentModule } from './rent/rent.module';
-import { ImagesModule } from './images/images.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     RentProductsModule,
     FeedbackModule,
     RentModule,
-    ImagesModule,
-    DatasourceModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASS,
+      database: process.env.DATABASE_BASE,
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
   ],
   controllers: [],
   providers: [
