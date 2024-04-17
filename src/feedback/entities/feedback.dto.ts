be4556382f9940ce8ff0381435ebb896483ab413
KnightParzivalll <koffection@gmail.com>
2024-04-17 20:18:41 +0300

    [with_mysql] hotfix: add updated types for feedback module

--- src/feedback/entities/feedback.dto.ts
+++ src/feedback/entities/feedback.dto.ts
@@ -1,15 +1,35 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsIn } from 'class-validator';
+import { Gender, genderValues } from './feedback.entity';
 
 export class CreateFeedbackDto {
   @IsString()
+  @ApiProperty({
+    description: `First name of the user`,
+    example: 'James',
+  })
   first_name: string;
 
   @IsString()
+  @ApiProperty({
+    description: `Last name of the user`,
+    example: 'Roberts',
+  })
   last_name: string;
 
   @IsString()
+  @ApiProperty({
+    description: `Feedback text`,
+    example: 'Some good text',
+  })
   text: string;
 
-  @IsIn(['male', 'female'])
-  gender: string;
+  @IsIn(genderValues)
+  @ApiProperty({
+    enum: genderValues,
+    default: 'unknown',
+    description: `A gender of the user`,
+    example: 'male',
+  })
+  gender: Gender;
 }
--- src/feedback/entities/feedback.entity.ts
+++ src/feedback/entities/feedback.entity.ts
@@ -1,18 +1,49 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+export type Gender = 'male' | 'female' | 'unknown';
+export const genderValues = ['male', 'female', 'unknown'];
+
+@Entity('feedback')
 export class Feedback {
-  @ApiProperty()
+  @ApiProperty({
+    description: `id`,
+    example: '1',
+  })
+  @PrimaryGeneratedColumn()
   id: number;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: `First name of the user`,
+    example: 'James',
+  })
+  @Column({ length: 50 })
   first_name: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: `Last name of the user`,
+    example: 'Roberts',
+  })
+  @Column({ length: 50 })
   last_name: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: `Feedback text`,
+    example: 'Some good text',
+  })
+  @Column({ length: 500 })
   text: string;
 
-  @ApiProperty()
-  gender: string;
+  @ApiProperty({
+    enum: genderValues,
+    default: 'unknown',
+    description: `A gender of the user`,
+    example: 'male',
+  })
+  @Column({
+    type: 'enum',
+    enum: genderValues,
+    default: 'unknown',
+  })
+  gender: Gender;
 }
